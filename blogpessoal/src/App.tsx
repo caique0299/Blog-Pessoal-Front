2f4a68d89e8e798e37860cbffcfbe36fadccd6dd
Elisangela <caiqueramoss@hotmail.com>
2023-06-22 18:38:35 -0300

    Login e App

---
+++ blogpessoal/src/App.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Navbar from "./components/estaticos/navbar/Navbar";
+import Footer from "./components/estaticos/footer/Footer";
+import "./App.css";
+import Home from "./paginas/home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./paginas/login/Login";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <div style={{ minHeight: "100vh" }}>
+        <Routes>
+          {" "}
+          // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          {/*<Route path="/cadastro" element={<CadastroUsuario />} /> */}
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+{
+  /*<Grid container spacing={3}>
+        <Grid item xs={12} sm={12}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+  </Grid> */
+}
