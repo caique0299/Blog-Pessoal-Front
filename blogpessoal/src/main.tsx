4e5ad1655a3edb10bf071b46a85dd273a055fcb5
Elisangela <caiqueramoss@hotmail.com>
2023-07-04 09:14:47 -0300

    Configurações finais

--- blogpessoal/src/main.tsx
+++ blogpessoal/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.tsx";
 import "./index.css";
-import Home from "./pages/home.tsx";
 
 ReactDOM.render(
   <React.StrictMode>
